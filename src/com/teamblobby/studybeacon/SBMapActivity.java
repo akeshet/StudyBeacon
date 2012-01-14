ca299b2a49485bd786c99f5f7cfbb1cac7c88feb
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-01-14 17:55:32 -0500

    First commit.

---
+++ src/com/teamblobby/studybeacon/SBMapActivity.java
@@ -0,0 +1,21 @@
+package com.teamblobby.studybeacon;
+
+import com.google.android.maps.MapActivity;
+//import com.google.android.maps.MapView;
+
+import android.os.Bundle;
+
+public class SBMapActivity extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		
+		return false;
+	}
+}
