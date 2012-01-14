e5fe7371005aef744a3567b2ae4a33d118733a7b
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-01-14 18:11:00 -0500

    make go

--- src/com/teamblobby/studybeacon/SBMapActivity.java
+++ src/com/teamblobby/studybeacon/SBMapActivity.java
@@ -1,7 +1,7 @@
 package com.teamblobby.studybeacon;
 
 import com.google.android.maps.MapActivity;
-//import com.google.android.maps.MapView;
+import com.google.android.maps.MapView;
 
 import android.os.Bundle;
 
@@ -11,6 +11,10 @@ public class SBMapActivity extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // add zoom
+	    MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
     }
 
 	@Override
