10bfcfe6d864dc2adeae49922d972bba7b1e64ef
Leo C. Stein <leostein@mit.edu>
2012-01-14 20:36:29 -0500

    Still trying to get the spinner to work

--- src/com/teamblobby/studybeacon/SBMapActivity.java
+++ src/com/teamblobby/studybeacon/SBMapActivity.java
@@ -1,12 +1,25 @@
 package com.teamblobby.studybeacon;
 
+import java.util.List;
+import java.util.Set;
+
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
 import android.os.Bundle;
+import android.text.TextUtils.StringSplitter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class SBMapActivity extends MapActivity {
-    /** Called when the activity is first created. */
+	
+	protected Spinner courseSpinner;
+	protected int courseSpinnerId;
+	
+	public final static String COURSES_STR = "courses";
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +28,29 @@ public class SBMapActivity extends MapActivity {
         // add zoom
 	    MapView mapView = (MapView) findViewById(R.id.mapview);
 	    mapView.setBuiltInZoomControls(true);
+	    
+	    // Set the default view?
+	    
+	    // Find the spinner
+	    courseSpinnerId = R.id.mapcoursespinner;
+	    courseSpinner = (Spinner) findViewById(courseSpinnerId);
+	    
+	    loadCourses();
+	    
+    }
+    
+    protected void loadCourses() {
+    	ArrayAdapter<CharSequence> courseSpinnerAdapter = new ArrayAdapter<CharSequence>(null, 0);
+    	
+    	String courseList = Global.prefs.getString(COURSES_STR, "6.570|8.901");
+
+    	String courses[] = courseList.split("|");
+    	
+    	for( String course : courses){
+    		courseSpinnerAdapter.add(course);
+    	}
+    	
+    	courseSpinner.setAdapter(courseSpinnerAdapter);
     }
 
 	@Override
