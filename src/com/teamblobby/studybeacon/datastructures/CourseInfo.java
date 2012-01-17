936344c6c1bf419b07d3dab62d256a958ecb5209
Aviv Keshet <akeshet@gmail.com>
2012-01-17 14:17:45 -0500

    Added some javadoc.

--- src/com/teamblobby/studybeacon/datastructures/CourseInfo.java
+++ src/com/teamblobby/studybeacon/datastructures/CourseInfo.java
@@ -1,5 +1,12 @@
 package com.teamblobby.studybeacon.datastructures;
 
+/**
+ * 
+ * Super class which defines the interface that CourseInfo implementations will follow.
+ * Implementations may either store course data locally within the object (such as {@link CourseInfoSimple})
+ * or in a local SQLite database (not yet implemented) or in a cache of data that was pulled from the MIT Stellar API.
+ *
+ */
 public abstract class CourseInfo {
 	
 	public abstract String getCourseName();
