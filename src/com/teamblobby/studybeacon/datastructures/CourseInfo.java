156adfb64820ca1d95c0b2ff9b33cfa2558a1922
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-01-18 16:08:58 -0500

    Make CourseInfo an implementation of Parcelable.

--- src/com/teamblobby/studybeacon/datastructures/CourseInfo.java
+++ src/com/teamblobby/studybeacon/datastructures/CourseInfo.java
@@ -1,5 +1,8 @@
 package com.teamblobby.studybeacon.datastructures;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * 
  * Super class which defines the interface that CourseInfo implementations will follow.
@@ -8,7 +11,7 @@ package com.teamblobby.studybeacon.datastructures;
  * MIT Stellar API (not yet implemented).
  *
  */
-public abstract class CourseInfo {
+public abstract class CourseInfo implements Parcelable {
 	
 	public abstract String getCourseName();
 	public abstract void setCourseName(String courseName);
@@ -31,4 +34,34 @@ public abstract class CourseInfo {
 		}
 		return outArray;
 	}
+	
+	// Implementations of parcelable interface for android friendly serialization
+	
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.getCourseName()); // first write course name
+		dest.writeBooleanArray(new boolean[] {this.getStarred(),this.getNotify()}); //then write array of starred/notify
+	}
+	
+	public static final Parcelable.Creator<CourseInfo> CREATOR = 
+									new Parcelable.Creator<CourseInfo>() {
+		
+			public CourseInfo createFromParcel(Parcel in){
+				String courseName = in.readString();
+				boolean[] arrayStarNotify = new boolean[2];
+				in.readBooleanArray(arrayStarNotify);
+				
+				return new CourseInfoSimple(courseName, arrayStarNotify[0], arrayStarNotify[1]);
+				
+			}
+
+			public CourseInfo[] newArray(int size) {
+				return new CourseInfo[size];
+			}
+			
+		};
 }
