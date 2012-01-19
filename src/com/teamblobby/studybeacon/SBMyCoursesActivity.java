d7aacb16b61ef963401b3d3f6289a00e867a4a6b
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-01-18 22:10:40 -0500

    Sad I had to turn Aviv's nice anonymous class into a named class, but I'm going to need it in the class picker activity.

--- src/com/teamblobby/studybeacon/SBMyCoursesActivity.java
+++ src/com/teamblobby/studybeacon/SBMyCoursesActivity.java
@@ -1,9 +1,9 @@
 package com.teamblobby.studybeacon;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.*;
 import android.view.View.OnClickListener;
 import android.widget.*;
@@ -31,53 +31,60 @@ public class SBMyCoursesActivity extends ListActivity {
     	
      	CourseInfo [] courseInfos = (CourseInfo[]) Global.getCourseInfos();
     	
-    	ArrayAdapter<CourseInfo> courseAdapter = new ArrayAdapter<CourseInfo>(this, R.layout.mycoursesrow, R.id.mcrCourseNameTextView, courseInfos) {
-    		public View getView(int position, View convertView, ViewGroup parent) {
-    			View viewToReturn = super.getView(position, convertView, parent);
-    			
-    			final CourseInfo courseInfo = this.getItem(position);
-    			
-    			CheckBox notifyCheckBox = (CheckBox) viewToReturn.findViewById(R.id.mcrNotificationButton);
-    			notifyCheckBox.setOnCheckedChangeListener(null); // This is necessary in case we are recycling a previous "convertView"
-    															// so that its event hander gets un-hooked BEFORE we change the checked state
-    															// of the button (on the next line)
-    			notifyCheckBox.setChecked(courseInfo.getNotify());
-    			notifyCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-					
-					public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-						courseInfo.setNotify(isChecked);
-					}
-				});
-    			
-    			CheckBox starCheckBox = (CheckBox) viewToReturn.findViewById(R.id.mcrStarButton);
-    			starCheckBox.setOnCheckedChangeListener(null);
-    			starCheckBox.setChecked(courseInfo.getStarred());
-    			starCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-					
-					public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-						courseInfo.setStarred(isChecked);
-					}
-				});
-    			
-    			ImageButton newBeaconButton = (ImageButton) viewToReturn.findViewById(R.id.mcrNewBeaconButton);
-    			newBeaconButton.setOnClickListener(new OnClickListener() {
-					
-					public void onClick(View v) {
-						Intent i = new Intent(SBMyCoursesActivity.this, SBBeaconEditActivity.class);
-						SBMyCoursesActivity.this.startActivity(i);
-					}
-				});
-    			
-    			return viewToReturn;
-    			
-    		};
-    	};
-    	
-    	myListView.setAdapter(courseAdapter);
+    	myListView.setAdapter(new CourseAdapter(this,
+    											R.layout.mycoursesrow,
+    					                        R.id.mcrCourseNameTextView,
+    					                        courseInfos));
     	
     }
     
-    
+    protected class CourseAdapter extends ArrayAdapter<CourseInfo>{
+
+		public CourseAdapter(Context context, int resource,
+				int textViewResourceId, CourseInfo[] courseInfos) {
+			super(context, resource, textViewResourceId, courseInfos);
+		}
+    	
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View viewToReturn = super.getView(position, convertView, parent);
+			
+			final CourseInfo courseInfo = this.getItem(position);
+			
+			CheckBox notifyCheckBox = (CheckBox) viewToReturn.findViewById(R.id.mcrNotificationButton);
+			notifyCheckBox.setOnCheckedChangeListener(null); // This is necessary in case we are recycling a previous "convertView"
+															// so that its event hander gets un-hooked BEFORE we change the checked state
+															// of the button (on the next line)
+			notifyCheckBox.setChecked(courseInfo.getNotify());
+			notifyCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+				
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					courseInfo.setNotify(isChecked);
+				}
+			});
+			
+			CheckBox starCheckBox = (CheckBox) viewToReturn.findViewById(R.id.mcrStarButton);
+			starCheckBox.setOnCheckedChangeListener(null);
+			starCheckBox.setChecked(courseInfo.getStarred());
+			starCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+				
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					courseInfo.setStarred(isChecked);
+				}
+			});
+			
+			ImageButton newBeaconButton = (ImageButton) viewToReturn.findViewById(R.id.mcrNewBeaconButton);
+			newBeaconButton.setOnClickListener(new OnClickListener() {
+				
+				public void onClick(View v) {
+					Intent i = new Intent(SBMyCoursesActivity.this, SBBeaconEditActivity.class);
+					SBMyCoursesActivity.this.startActivity(i);
+				}
+			});
+			
+			return viewToReturn;
+			
+		};
+    }
     
 	private class CourseClickListener implements OnClickListener {
 		public void onClick(View view){
