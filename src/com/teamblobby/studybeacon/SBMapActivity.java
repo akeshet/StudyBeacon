f262bd5726615252f8171a0f5e5147264d370a6d
Aviv Keshet <akeshet@gmail.com>
2012-01-20 20:34:09 -0500

    When returning from My Courses activity to Map activity, the spinner should now be updated to reflect courses.

--- src/com/teamblobby/studybeacon/SBMapActivity.java
+++ src/com/teamblobby/studybeacon/SBMapActivity.java
@@ -21,96 +21,107 @@ import android.widget.Toast;
 public class SBMapActivity extends MapActivity implements SBAPIHandler
 {
 
+	public final static int REQUESTCODE_RETURNED_FROM_MYCOURSES = 1;
+
 	public final static String TAG = "SBMapActivity";
-	
+
 	protected Spinner courseSpinner;
 	protected int courseSpinnerId;
-	
+
 	protected Button myClassesButton;
-	
+
 	private MapView mapView;
 	private MapController mapViewController;
-	
+
 	protected HashMap<Integer, BeaconInfo> mBeacons;
-	
+
 	private List<Overlay> overlays;
 	private MyLocationOverlay myLocOverlay;
 	/** Hash from course string to itemized overlays */
 	private HashMap<String,BeaconItemizedOverlay> beacItemOverlays; 
 
 	private Drawable beaconD = Global.res.getDrawable(R.drawable.beacon);
-	
+
 	private String courses[];
-	
+
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.map);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	    // set up an onClickListener handler for classesButton
-	    myClassesButton = (Button) findViewById(R.id.myClassesButton);
-	    myClassesButton.setOnClickListener(new View.OnClickListener() {
-	    	public void onClick(View v) {
-	    		Intent i = new Intent(SBMapActivity.this, SBMyCoursesActivity.class);
-	    		startActivity(i);
-	    	}
-	    });
-        
-        this.setUpMapView(savedInstanceState);
-	    
-	    // Find the spinner
-	    courseSpinnerId = R.id.mapCourseSpinner;
-	    courseSpinner = (Spinner) findViewById(courseSpinnerId);
-	    
-	    this.loadCourses(savedInstanceState);
+		setContentView(R.layout.map);
 
-	    this.setUpBeacons(savedInstanceState);
-	    
-    }
+		// set up an onClickListener handler for classesButton
+		myClassesButton = (Button) findViewById(R.id.myClassesButton);
+		myClassesButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(SBMapActivity.this, SBMyCoursesActivity.class);
+				startActivityForResult(i, REQUESTCODE_RETURNED_FROM_MYCOURSES);
+			}
+		});
 
-    @Override
-    public void onResume() {
-    	super.onResume();
-    	Log.d(TAG,"onResume()");
-    	myLocOverlay.enableMyLocation();
-    }
-    
-    @Override
-    public void onPause() {
-    	super.onPause();
-    	Log.d(TAG,"onPause()");
-    	myLocOverlay.disableMyLocation();
-    }
-    
-	protected void setUpMapView(Bundle savedInstanceState) {
-    	// TODO use savedInstanceState if possible
-		// add zoom
-	    this.mapView = (MapView) findViewById(R.id.mapView);
-	    mapView.setBuiltInZoomControls(true);
-	    
-	    // get the mapview controller, set the default map location on MIT campus
-	    this.mapViewController = mapView.getController();
-	    
-	    // get the overlays
-	    this.overlays = mapView.getOverlays();
-	    // Add a MyLocationOverlay to it
-	    this.myLocOverlay = new MyLocationOverlay(this,mapView);
-	    
-	    overlays.add(myLocOverlay);
-	    
-	    // Nic is so smart! -Leo
-        if (savedInstanceState == null) this.setMapPosition(); 
-	    
+		this.setUpMapView(savedInstanceState);
+
+		// Find the spinner
+		courseSpinnerId = R.id.mapCourseSpinner;
+		courseSpinner = (Spinner) findViewById(courseSpinnerId);
+
+		this.loadCourses(savedInstanceState);
+
+		this.setUpBeacons(savedInstanceState);
+
 	}
-	
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG,"onResume()");
+		myLocOverlay.enableMyLocation();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG,"onPause()");
+		myLocOverlay.disableMyLocation();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUESTCODE_RETURNED_FROM_MYCOURSES:
+			loadCourses(null);
+			break;
+		}
+	}
+
+	protected void setUpMapView(Bundle savedInstanceState) {
+		// TODO use savedInstanceState if possible
+		// add zoom
+		this.mapView = (MapView) findViewById(R.id.mapView);
+		mapView.setBuiltInZoomControls(true);
+
+		// get the mapview controller, set the default map location on MIT campus
+		this.mapViewController = mapView.getController();
+
+		// get the overlays
+		this.overlays = mapView.getOverlays();
+		// Add a MyLocationOverlay to it
+		this.myLocOverlay = new MyLocationOverlay(this,mapView);
+
+		overlays.add(myLocOverlay);
+
+		// Nic is so smart! -Leo
+		if (savedInstanceState == null) this.setMapPosition(); 
+
+	}
+
 	protected void setMapPosition() {
 		// begin at MIT
-	    mapViewController.setCenter(new GeoPoint( Global.res.getInteger(R.integer.mapDefaultLatE6),Global.res.getInteger(R.integer.mapDefaultLongE6)));
-	    mapViewController.setZoom(Global.res.getInteger(R.integer.mapDefaultZoom));
-	    // show user's position
-	    myLocOverlay.runOnFirstFix(new Runnable() {
+		mapViewController.setCenter(new GeoPoint( Global.res.getInteger(R.integer.mapDefaultLatE6),Global.res.getInteger(R.integer.mapDefaultLongE6)));
+		mapViewController.setZoom(Global.res.getInteger(R.integer.mapDefaultZoom));
+		// show user's position
+		myLocOverlay.runOnFirstFix(new Runnable() {
 			public void run() {
 				mapViewController.animateTo(myLocOverlay.getMyLocation());
 			}});
@@ -119,59 +130,59 @@ public class SBMapActivity extends MapActivity implements SBAPIHandler
 	private void setUpBeacons(Bundle savedInstanceState) {
 		// TODO use savedInstanceState
 		mBeacons = new HashMap<Integer, BeaconInfo>();
-		
-	    beacItemOverlays = new HashMap<String,BeaconItemizedOverlay>();
-	    
-//	    beacItemOverlay.addOverlay(new BeaconOverlayItem(mapView.getMapCenter(), "test", "123",null));
-	    
-//	    overlays.add(beacItemOverlay);
-	    
-	    startQuery();
+
+		beacItemOverlays = new HashMap<String,BeaconItemizedOverlay>();
+
+		//	    beacItemOverlay.addOverlay(new BeaconOverlayItem(mapView.getMapCenter(), "test", "123",null));
+
+		//	    overlays.add(beacItemOverlay);
+
+		startQuery();
 	}
 
 	private void startQuery() {
 		// find the view bounds
-	    int deltaLat = mapView.getLatitudeSpan(), deltaLon = mapView.getLongitudeSpan();
-	    GeoPoint ctr = mapView.getMapCenter();
-	    int LatE6Min, LatE6Max, LonE6Min, LonE6Max;
-	    LatE6Min = ctr.getLatitudeE6() - deltaLat/2;
-	    LatE6Max = ctr.getLatitudeE6() + deltaLat/2;
-	    LonE6Min = ctr.getLongitudeE6() - deltaLon/2;
-	    LonE6Max = ctr.getLongitudeE6() + deltaLon/2;
-	    
-	    // Try to populate the beacons asynchronously
-	    // TODO don't use courses, but rather the selection from the spinner.
-	    // If the selection is All, then use courses.
-	    APIClient.query(LatE6Min, LatE6Max, LonE6Min, LonE6Max, courses, this);
-	}
-    
-    protected void loadCourses(Bundle savedInstanceState) {
-    	// TODO use savedInstanceState if possible
-    	
-    	ArrayAdapter<CharSequence> courseSpinnerAdapter =
-    			new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+		int deltaLat = mapView.getLatitudeSpan(), deltaLon = mapView.getLongitudeSpan();
+		GeoPoint ctr = mapView.getMapCenter();
+		int LatE6Min, LatE6Max, LonE6Min, LonE6Max;
+		LatE6Min = ctr.getLatitudeE6() - deltaLat/2;
+		LatE6Max = ctr.getLatitudeE6() + deltaLat/2;
+		LonE6Min = ctr.getLongitudeE6() - deltaLon/2;
+		LonE6Max = ctr.getLongitudeE6() + deltaLon/2;
 
-    	courseSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    	
-    	courses = CourseInfo.getCourseNames(Global.getMyCourseInfos());
-    	
-    	courseSpinnerAdapter.add(getString(R.string.allCourses));
-    	for( String course : courses){
-    		courseSpinnerAdapter.add(course);
-    	}
-    	courseSpinnerAdapter.add(getString(R.string.editCourses));
-    	
-    	// TODO Add a handler for detecting "All" and "Edit"
-    	
-    	courseSpinner.setAdapter(courseSpinnerAdapter);
-    }
+		// Try to populate the beacons asynchronously
+		// TODO don't use courses, but rather the selection from the spinner.
+		// If the selection is All, then use courses.
+		APIClient.query(LatE6Min, LatE6Max, LonE6Min, LonE6Max, courses, this);
+	}
+
+	protected void loadCourses(Bundle savedInstanceState) {
+		// TODO use savedInstanceState if possible
+
+		ArrayAdapter<CharSequence> courseSpinnerAdapter =
+				new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+
+		courseSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		courses = CourseInfo.getCourseNames(Global.getMyCourseInfos());
+
+		courseSpinnerAdapter.add(getString(R.string.allCourses));
+		for( String course : courses){
+			courseSpinnerAdapter.add(course);
+		}
+		courseSpinnerAdapter.add(getString(R.string.editCourses));
+
+		// TODO Add a handler for detecting "All" and "Edit"
+
+		courseSpinner.setAdapter(courseSpinnerAdapter);
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
-		
+
 		return false;
 	}
-	
+
 	public void newBeaconClicked(View view) {
 		Intent i = new Intent(this, SBBeaconEditActivity.class);
 		startActivity(i);
@@ -187,21 +198,21 @@ public class SBMapActivity extends MapActivity implements SBAPIHandler
 		for (BeaconInfo beacon : beacons) {
 
 			Boolean addOverlay = false;
-			
+
 			String courseName = beacon.getCourseName();
-			
+
 			BeaconInfo old = this.mBeacons.put(beacon.getBeaconId(), beacon);
-			
+
 			BeaconItemizedOverlay courseOverlay;
-			
+
 			if (old != null) {
 				// this is updated info about a beacon we already knew about
 				// remove from the overlay that has it
 				// Since they have the same ID, courseName is the same
 				this.beacItemOverlays.get(courseName).removeByBeaconId(old.getBeaconId());
-				
+
 			}
-			
+
 			// Check if there is an overlay for this beacon's course
 			if ( this.beacItemOverlays.containsKey(courseName)) {
 				// One already exists
@@ -213,21 +224,21 @@ public class SBMapActivity extends MapActivity implements SBAPIHandler
 				// Make sure to add it later
 				addOverlay = true;
 			} 
-			
+
 			// Add a beacon to the itemized.
 			BeaconOverlayItem item = new BeaconOverlayItem(beacon);
-			
+
 			courseOverlay.addOverlay(item);
-			
+
 			if (addOverlay) {
 				this.overlays.add(courseOverlay);
 			}
-			
+
 			//			String snippet = Integer.toString(beacon.getVisitors()) + " visitors";
-//			OverlayItem item = new OverlayItem(beacon.getLoc(), beacon.getCourseName(), snippet);
-//			beacItemOverlay.addOverlay(item);
+			//			OverlayItem item = new OverlayItem(beacon.getLoc(), beacon.getCourseName(), snippet);
+			//			beacItemOverlay.addOverlay(item);
 		}
-			
+
 	}
 
 	public void onQueryFailure() {
