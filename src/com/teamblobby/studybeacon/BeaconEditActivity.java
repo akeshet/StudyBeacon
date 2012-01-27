057ff0e1b47660841c82b969341f88eab2946826
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-01-27 15:05:59 -0500

    delint

--- src/com/teamblobby/studybeacon/BeaconEditActivity.java
+++ src/com/teamblobby/studybeacon/BeaconEditActivity.java
@@ -21,7 +21,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
