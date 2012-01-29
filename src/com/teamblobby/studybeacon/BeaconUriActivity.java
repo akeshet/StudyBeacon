f69df10540d101b7ee33f954e1ce67b8bf07355d
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-01-28 20:44:12 -0500

    handle join fails.

--- src/com/teamblobby/studybeacon/BeaconUriActivity.java
+++ src/com/teamblobby/studybeacon/BeaconUriActivity.java
@@ -91,7 +91,16 @@ public class BeaconUriActivity extends Activity implements APIHandler{
 
 	public void onFailure(APICode code, Throwable e) {
 		dialog.dismiss();
-		Toast.makeText(this, "Problem communicating with server.", Toast.LENGTH_SHORT).show();
+		switch (code) {
+		case CODE_GETBEACON:
+			Toast.makeText(this, "Problem communicating with server.", Toast.LENGTH_SHORT).show();
+			break;
+		case CODE_JOIN:
+			Toast.makeText(this, "Could not join beacon.", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			break;
+		}
 		finish();
 	}
 
